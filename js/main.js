52a843af8d8d6bcd16094726a1625fa6a3a6e310
John Doe <johndoe@email.com>
2024-03-24 17:29:57 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+   // Initialize Swiper
+   var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3, // Number of slides per view
+    spaceBetween: 20, // Space between slides (adjust as needed)
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
